885c30fd3d5fa987412eab1147bb3f0dbe69d176
Arne Kleinschmidt <arne.kleinschmidt@uxi.de>
2018-06-17 14:28:28 +0200

    removes obsolete non-react-hmr code

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { hot } from 'react-hot-loader'
 
 const App = () => (
-  <h1>AGAIN!!!</h1>
+  <h1>I am App!</h1>
 )
 
 export default hot(module)(App);
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,8 @@ import * as React from "react";
 import { render } from "react-dom";
 
 import App from "./containers/App";
-import Hello from "./components/Hello/Hello";
 
 render(
   <App />,
   document.getElementById('app')
 );
-
-if (module.hot) {
-    module.hot.accept();
-}
