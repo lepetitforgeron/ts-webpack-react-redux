b5e6033c5e2b3b37324c0836f1e4197893184f2d
Arne Kleinschmidt <arne.kleinschmidt@uxi.de>
2018-06-17 14:31:28 +0200

    adds a greeting from our initial component

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react'
 import { hot } from 'react-hot-loader'
+import Hello from '../components/Hello/Hello';
 
 const App = () => (
-  <h1>I am App!</h1>
+  <div>
+    <h1>I am App!</h1>
+    <Hello compiler="Typescript with Webpack" framework="React" />
+  </div>
 )
 
 export default hot(module)(App);
